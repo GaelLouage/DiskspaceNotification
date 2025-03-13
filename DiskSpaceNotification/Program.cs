1fd46f657a59df74143f715e6d88fbb61bc7e1e4
Gaël Louage <louagegael@hotmail.comgit cleargit config --global user.email louagegael@hotmail.com>
2025-03-13 18:31:43 +0100

    diskspace notificition

---
+++ DiskSpaceNotification/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System.Timers;
+
+internal class Program
+{
+    private static System.Timers.Timer aTimer;
+    private static DateTime _timeTreshold;
+    private static bool _isNotificationCalled;
+    private static void Main(string[] args)
+    {
+        SetTimer();
+        Console.ReadLine();
+    }
+
+    private static void SetTimer()
+    {
+    
+        _timeTreshold = DateTime.Today.AddHours(18).AddMinutes(28);
+  
+        // Create a timer with a two second interval.
+        aTimer = new System.Timers.Timer(1000);
+
+        aTimer.Start();
+        // Hook up the Elapsed event for the timer. 
+        aTimer.Elapsed += OnTimerEvent;
+        aTimer.AutoReset = true;
+        aTimer.Enabled = true;
+   
+    }
+
+    private static void OnTimerEvent(object sender, EventArgs e)
+    {
+        if(DateTime.Now >= _timeTreshold && _isNotificationCalled is false)
+        {
+            _isNotificationCalled = true;
+            var (totalDiskSpace, totalFreeSpace) = GetDriveData("C:\\");
+            new ToastContentBuilder()
+               .AddArgument("DiskData", "")
+               .AddArgument("conversationId", 9813)
+               .AddText($"Diskspace: {totalDiskSpace.ToString("F2")}GB\nFreeSpace: {totalFreeSpace.ToString("F2")}GB")
+               .Show(); // Not seeing the Show() method? Make sure you have version 7.0, and if you're using .NET 6 (or later), then your TFM must be net6.0-windows10.0.17763.0 or greater
+        }
+    }
+    private static (double TotalDiskSpace, double TotalFreeSpace) GetDriveData(string driveName)
+    {
+        foreach (DriveInfo drive in DriveInfo.GetDrives())
+        {
+            if (drive.IsReady && drive.Name == driveName)
+            {
+                return (drive.TotalSize / Math.Pow(1024, 3), drive.TotalFreeSpace / Math.Pow(1024, 3));
+            }
+        }
+        return (-1, -1);
+    }
+}
